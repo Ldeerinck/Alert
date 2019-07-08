f335d69be9390caf3f7b32c6642bce3066f170b8
LDeerinck <ldeerinck2@me.com>
2019-07-08 15:10:36 -0700

    update

--- Alert/EventViewController.swift
+++ Alert/EventViewController.swift
@@ -8,23 +8,85 @@
 
 import UIKit
 
-class EventViewController: UIViewController {
+class EventViewController: UITableViewController, UIPickerViewDelegate {
+    
+    var index:Int = 0
+    var shouldBeHidden:Bool = true
+    var dPicker:UIDatePicker!
+    lazy var recurrenceDataSource = RecurrenceType(events[index])
+    lazy var textDelegate = TextViewDelegate(events[index])
+    @IBOutlet var eventTableView: UITableView!
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        eventTableView.backgroundView = UIImageView(image: events[index].picture)
+        eventTableView.tableFooterView = UIView(frame: CGRect.zero)
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+        
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 0: return 45
+        case 1: if shouldBeHidden {return 0} else {return 95}
+        case 2: return 200
+        case 3: return 95
+        default: return 45
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            shouldBeHidden.toggle()
+            let pickerPath = IndexPath(row:1,section:0)
+            tableView.reloadRows(at: [pickerPath], with: .automatic)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell:UITableViewCell
+        switch indexPath.row {
+        case 0:
+            cell = tableView.dequeueReusableCell(withIdentifier: "dateDisplay", for: indexPath)
+            if let label = cell.viewWithTag(1) as? UILabel {
+                let dateFormatter = DateFormatter()
+                dateFormatter.dateStyle = .medium
+                dateFormatter.timeStyle = .none
+                label.text = dateFormatter.string(from: events[index].date)
+            }
+        case 1:
+            cell = tableView.dequeueReusableCell(withIdentifier: "dateCell", for: indexPath)
+            if let label = cell.viewWithTag(2) as? UIDatePicker {
+                label.date = events[index].date
+                dPicker = label
+            }
+        case 2:
+            cell = tableView.dequeueReusableCell(withIdentifier: "description cell", for: indexPath)
+            if let label = cell.viewWithTag(3) as? UITextView {
+                label.text = events[index].description
+                label.delegate = textDelegate
+                
+            }
+        case 3:
+            cell = tableView.dequeueReusableCell(withIdentifier: "picker cell", for: indexPath)
+            if let label = cell.viewWithTag(5) as? UIPickerView {
+                label.dataSource = recurrenceDataSource
+                label.delegate = recurrenceDataSource
+            }
+        default:
+            cell = tableView.dequeueReusableCell(withIdentifier: "picture cell", for: indexPath)
+        }
+        return cell
+    }
+    
+    @IBAction func datedidChange(_ sender: Any) {
+        events[index].date = dPicker.date
+        let pickerPath = IndexPath(row:0,section:0)
+        tableView.reloadRows(at: [pickerPath], with: .automatic)
+    }
+    
 }
--- Alert/MainViewController.swift
+++ Alert/MainViewController.swift
@@ -9,20 +9,29 @@
 import UIKit
 
 class MainViewController: UITableViewController {
-
+    
+    var eventHub:Events = Events()
+    var index:Int = 0
+    @IBOutlet var summaryTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         testData()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        eventHub.sort()
+        summaryTable.reloadData()
+    }
+    
     func testData() {
         let currDate = Date();
-        var temp = Event(description: "run 10k", date: currDate, type: .repeatDays, days: 5, months: nil, picture: nil)
+        var temp = Event(description: "run 10k", date: currDate, type: .repeatDays, duration: 5, picture: nil)
         events.append(temp)
-         temp = Event(description: "Sleep", date: currDate, type: .repeatDays, days: 5, months: nil, picture: nil)
+         temp = Event(description: "Sleep", date: currDate, type: .repeatDays, duration: 5, picture: nil)
         events.append(temp)
-         temp = Event(description: "Program", date: currDate, type: .repeatDays, days: 5, months: nil, picture: nil)
+         temp = Event(description: "Program", date: currDate, type: .repeatDays, duration: 5, picture: nil)
         events.append(temp)
     }
 
@@ -42,13 +51,25 @@ class MainViewController: UITableViewController {
             //dateFormatter.locale = Locale(identifier: "en_US")
             label.text = dateFormatter.string(from: events[indexPath.row].date)
         }
+        if let label = cell.viewWithTag(3) as? UILabel {
+            label.text = "\(events[indexPath.row].description)"
+        }
         let imageView = UIImageView(image: events[indexPath.row].picture)
         cell.backgroundView = imageView
         cell.backgroundView?.contentMode = .scaleAspectFill
         return cell
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.destination is EventViewController
+        {
+            let vc = segue.destination as? EventViewController
+            vc?.index = self.index
+        }
+    }
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        index = indexPath.row
         performSegue(withIdentifier: "eventClicked", sender: self)
     }
 }
